a6fe9c70f00805b5959aa874a098cd13d2b16abb
luljn <mbohlulajonathan4@gmail.com>
2024-01-21 21:44:31 +0100

    3 elements added to the Freelances page with Card component

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,35 @@
+import Card from "../../components/Card"
+
+const freelanceProfiles = [
+    {
+        name: 'Jane Doe',
+        jobTitle: 'DevOps',
+        picture: "C:\Users\User\Documents\X2026\X2 2022-2023\Stage de découverte\IPM (Internship Projects Manager)\static app\X\IPM\media\defaultUserPicture.png",
+    },
+    {
+        name: 'John Doe',
+        jobTitle: 'Développeur frontend',
+        picture: "C:\Users\User\Documents\X2026\X2 2022-2023\Stage de découverte\IPM (Internship Projects Manager)\static app\X\IPM\media\defaultUserPicture.png",
+    },
+    {
+        name: 'Jeanne Biche',
+        jobTitle: 'Développeur Fullstack',
+        picture: "C:\Users\User\Documents\X2026\X2 2022-2023\Stage de découverte\IPM (Internship Projects Manager)\static app\X\IPM\media\defaultUserPicture.png",
+    }
+]
+
 function Freelances() {
     return (
         <div>
             <h1>Freelances 👨🏾‍💻👨🏾‍💻👨🏾‍💻</h1>
+            {freelanceProfiles.map((profile, index) => (
+                <Card
+                    key={`${profile.name}-${index}`}
+                    label={profile.jobTitle}
+                    picture={profile.picture}
+                    title={profile.name}
+                />
+            ))}
         </div>
     )
 }

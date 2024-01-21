10fe0500e11f469405db1bcdc1d14bc6d1de2e5f
luljn <mbohlulajonathan4@gmail.com>
2024-01-21 20:04:24 +0100

    Link to the Freelances page added to the navbar in the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,6 +6,7 @@ function Header() {
         <nav>
             <Link to="/">Acceuil</Link>
             <Link to="/survey/42">Questionnaire</Link>
+            <Link to="/freelances">Freelances</Link>
         </nav>
     )
 }

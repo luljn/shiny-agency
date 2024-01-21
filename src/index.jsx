3c404614c580ca91627d5a94f7558c0fbe1f0a74
luljn <mbohlulajonathan4@gmail.com>
2024-01-21 20:02:29 +0100

    Freelances page added to the router

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
+import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
 
@@ -18,6 +19,7 @@ root.render(
         <Route path='/' element={<Home />}/>
         <Route path='/survey/:questionNumber' element={<Survey />}/>
         <Route path='/results' element={<Results />}/>
+        <Route path='freelances' element={<Freelances />}/>
         {/* To redirect to an error page, 
         if the specified url does not exist  */}
         <Route path='*' element={<Error />}/>

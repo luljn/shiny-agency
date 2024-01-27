f76f7d76368b9bb5a63e349c99c788d9d1367fa7
luljn <mbohlulajonathan4@gmail.com>
2024-01-27 20:20:57 +0100

    Default props added to all the composants of Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import DefaultPicture from '../../assets/defaultUserPicture.png'
 
 function Card({ label, title, picture }) {
     return (
@@ -11,13 +12,15 @@ function Card({ label, title, picture }) {
 }
 
 Card.propTypes = {
-    label: PropTypes.string,
+    label: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    picture: PropTypes.string,
+    picture: PropTypes.string.isRequired,
 }
 
 Card.defaultProps = {
-    title: 'Mon titre par défaut',
+    label : '',
+    title: '',
+    picture: DefaultPicture,
 }
 
 export default Card

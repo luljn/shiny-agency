860a12ca6abb1f8c50cf6551a2fe2a039cc9d59a
luljn <mbohlulajonathan4@gmail.com>
2024-01-28 17:05:15 +0100

    Style added to Card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,24 @@
 import PropTypes from 'prop-types'
-import DefaultPicture from '../../assets/defaultUserPicture.png'
+import styled from 'styled-components'
+import DefaultPicture from "C:/Web projects/React Learning/shiny-agency/src/assets/defaultUserPicture.png"
+
+const CardLabel = styled.span`
+            color: #5843e4;
+            font-size: 22px;
+            font-weight: bold;
+        `
+
+const CardImage = styled.img`
+        height: 80px;
+        width: 80px;
+        border-radius: 50%;
+    `
 
 function Card({ label, title, picture }) {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-            <span>{label}</span>
-            <img src={picture} alt="freelance" height={80} width={80}/>
+            <CardLabel>{label}</CardLabel>
+            <CardImage src={picture} />
             <span>{title}</span>
         </div>
     )

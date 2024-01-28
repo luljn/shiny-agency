0ccf4e05a4edc64dec313d33e6f358f8dcbc2dd0
luljn <mbohlulajonathan4@gmail.com>
2024-01-28 17:41:22 +0100

    Style added to Home page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,6 +6,9 @@ const StyledLink = styled(Link)`
             color: #8186a0;
             text-decoration: none;
             font-size: 30px;
+            ${(props) => 
+                props.$isFullLink &&
+                `color: white; border-radius: 30px; background-color: #5843E4;`}
 `
 
 function Header() {
@@ -13,7 +16,9 @@ function Header() {
     return (
         <nav> 
             <StyledLink to="/">Acceuil</StyledLink>
-            <StyledLink to="/survey/1">Questionnaire</StyledLink>
+            <StyledLink to="/survey/1" $isFullLink>
+                Faire le test
+            </StyledLink>
             <StyledLink to="/freelances">Freelances</StyledLink>
         </nav>
     )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,27 @@
+// import { useState } from "react"
+import styled from "styled-components"
+
+const HomeContainer = styled.div`
+      width: 100%;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+`
+const Balloon = styled.div`
+      height: 100px;
+      width: 100px;
+      border-radius: 50px;
+      background-color: #20202;
+`
+
 function Home() {
 
-  return <div>Page d'accueil 🏡</div>
+  return (
+    <HomeContainer>
+      <h1>Page d'accueil 🏡</h1>
+      <Balloon />
+    </HomeContainer>
+  )
 }
 
 

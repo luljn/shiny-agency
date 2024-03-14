6926521edb38b1049e1c03d0c28633ffbae522c9
luljn <mbohlulajonathan4@gmail.com>
2024-03-14 22:08:20 +0100

    GlobalStyle Component added

---
+++ src/utils/style/GlobalStyle.jsx
@@ -0,0 +1,31 @@
+import { useContext } from "react"
+import { ThemeContext } from "../context/ThemeProvider"
+import { createGlobalStyle } from "styled-components"
+
+
+
+const StyledGlobalStyle = createGlobalStyle`
+    * {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+
+    body {
+        /* Ici cette syntaxe revient au même que
+        background-color: ${({ props }) =>
+        props.isDarkMode ? '#2F2E41' : 'white'};
+        */
+        background-color: ${({ isDarkMode }) => (isDarkMode ? 'black' : 'white')};
+        margin: 0;  
+    }
+`
+
+
+
+function GlobalStyle() {
+
+    const { theme } = useContext(ThemeContext)
+
+    return <StyledGlobalStyle isDarkMode={theme === 'dark'}/>
+}
+
+export default GlobalStyle

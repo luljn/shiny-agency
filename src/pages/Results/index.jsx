af5248382ddaa505b09dde2a05d37aba866c713a
luljn <mbohlulajonathan4@gmail.com>
2024-03-17 18:03:41 +0100

    Page Result integrated with useFetch and useTheme hooks

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -2,9 +2,8 @@ import { useContext } from 'react'
 import { SurveyContext } from '../../utils/context'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
-import { useFetch } from '../../utils/hooks'
+import { useFetch, useTheme } from '../../utils/hooks'
 import { StyledLink, Loader } from '../../utils/style/Atom'
-import { ThemeContext } from '../../utils/context'
 
 const ResultsContainer = styled.div`
   display: flex;
@@ -66,7 +65,7 @@ function formatFetchParams(answers) {
 
 function Results() {
   
-  const { theme } = useContext(ThemeContext)
+  const { theme } = useTheme()
   const { answers } = useContext(SurveyContext)
   const fetchParams = formatFetchParams(answers)
 
